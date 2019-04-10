36e5d864ee90b4d88d59a4c6b03d168afbbfc528
Socaci Radu Andrei <radusocaci@gmail.com>
2019-04-10 11:51:05 +0300

    Added connection

---
+++ src/Data_Access/Connection/ConnectionFactory.java
@@ -0,0 +1,66 @@
+package Data_Access.Connection;
+
+import java.sql.*;
+
+public class ConnectionFactory {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DBURL = "jdbc:mysql://localhost:3306/warehouse";
+    private static final String USER = "root";
+    private static final String PASS = "radutare";
+
+    private static ConnectionFactory singleInstance = new ConnectionFactory();
+
+    private ConnectionFactory() {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return singleInstance.createConnection();
+    }
+
+    public static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                //
+            }
+        }
+    }
+
+    public static void close(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                //
+            }
+        }
+    }
+
+    public static void close(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                //
+            }
+        }
+    }
+
+    private Connection createConnection() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DBURL, USER, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
---
+++ src/Data_Access/DAO/AbstractDAO.java
@@ -0,0 +1,4 @@
+package Data_Access.DAO;
+
+public class AbstractDAO {
+}
